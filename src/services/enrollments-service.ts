21140b906224b12faa871c21221f3e12036cca15
MarcusDantas <marvinsd@gmail.com>
2023-09-17 14:51:41 -0300

    fix: change error type when address is not present

--- src/services/enrollments-service.ts
+++ src/services/enrollments-service.ts
@@ -1,6 +1,6 @@
 import { Address, Enrollment } from '@prisma/client';
 import { request } from '@/utils/request';
-import { notFoundError, invalidDataError } from '@/errors';
+import { invalidDataError } from '@/errors';
 import { addressRepository, CreateAddressParams, enrollmentRepository, CreateEnrollmentParams } from '@/repositories';
 import { exclude } from '@/utils/prisma-utils';
 import { AddressFromCEP } from '@/protocols';
@@ -28,7 +28,7 @@ async function getAddressFromCEP(cep: string): Promise<AddressFromCEP> {
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
   const enrollmentWithAddress = await enrollmentRepository.findWithAddressByUserId(userId);
 
-  if (!enrollmentWithAddress) throw notFoundError();
+  if (!enrollmentWithAddress) throw invalidDataError('Address');
 
   const [firstAddress] = enrollmentWithAddress.Address;
   const address = getFirstAddress(firstAddress);
